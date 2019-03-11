112d3136da36b841dd35bb8264d04038677834fc
mirko vaia <mirko vaia@172.21.56.86>
2019-03-11 13:52:43 +0100

    Aggiunti metodi standard

--- src/it/polito/tdp/libretto/Voti.java
+++ src/it/polito/tdp/libretto/Voti.java
@@ -6,6 +6,30 @@ public class Voti {
 	private int voto;
 	private String corso;
 	private LocalDate data;
+	public Voti(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
 	
 	
 	
