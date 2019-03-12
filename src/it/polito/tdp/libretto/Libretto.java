a81733bef32bab2a525604cc52d05c3f1b96d02b
mirko vaia <mirko vaia@172.21.56.86>
2019-03-12 09:07:21 +0100

    implementati i metodi add della classe Libretto

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,18 +1,30 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Libretto {
 
-	List<Voti> voti;
+	private List<Voti> voti;
 
-	public Libretto(List<Voti> voti) {
+	public Libretto() {
 		super();
-		this.voti =new ArrayList<Voti>();
+		this.voti =new ArrayList<Voti>();}
+		
+	
+	
+	
+	public void add(int voto,String corso,LocalDate data) {
+		
 		
 	}
 	
+
+	public void add(Voti v) {
+		
+		
+	}
 	
 	
 	
