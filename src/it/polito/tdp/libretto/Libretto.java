8a123768dd4ef151d14a075502eded1589f99977
mirko vaia <mirko vaia@172.21.56.86>
2019-03-12 09:01:44 +0100

    fatta lista di voti in libretto

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,20 @@
 package it.polito.tdp.libretto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
 
+	List<Voti> voti;
+
+	public Libretto(List<Voti> voti) {
+		super();
+		this.voti =new ArrayList<Voti>();
+		
+	}
+	
+	
+	
+	
+	
 }
